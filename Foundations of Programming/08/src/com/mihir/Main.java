b2a2837f420f3969ab770b6a08491fe7f69956d7
13MB <mihirmehta.mehta@gmail.com>
2019-08-08 10:06:22 +0530

    Started with 8th Problem
    Given a non-empty array, return true if there is a place to split the array so that the sum of the numbers on one side is equal to the sum of the numbers on the other side.
    
    
    canBalance([1, 1, 1, 2, 1]) → true
    canBalance([2, 1, 1, 2, 1]) → false
    canBalance([10, 10]) → true

---
+++ Foundations of Programming/08/src/com/mihir/Main.java
@@ -0,0 +1,24 @@
+package com.mihir;
+
+
+/**
+ * Given a non-empty array, return true if there is a place to split the array so that the sum of the numbers on one side is equal to the sum of the numbers on the other side.
+ * <p>
+ * <p>
+ * canBalance([1, 1, 1, 2, 1]) → true
+ * canBalance([2, 1, 1, 2, 1]) → false
+ * canBalance([10, 10]) → true
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        int nums[] = {1, 1, 1, 2, 1};
+        System.out.println(new Main().canBalance(nums));
+    }
+
+    public boolean canBalance(int[] nums) {
+        boolean canBalance = false;
+
+        return canBalance;
+    }
+}
